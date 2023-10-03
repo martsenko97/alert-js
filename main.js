70198f23ab3618df392537fe136ab2d2bb88d55e
martsenko97 <amarcenko1997@gmail.com>
2023-10-03 10:40:09 +0300

    My first code js

---
+++ main.js
@@ -0,0 +1,9 @@
+'use strict'
+
+let string = 'Hello!'
+alert(string)
+
+let name = prompt('What is your name?', '')
+
+let fromCountry = prompt('Where are you from?')
+alert('Nice to meet you, ' + name + '! You are from ' + fromCountry + '!')
