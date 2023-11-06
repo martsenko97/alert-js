7e1c65a67e1bfea4f1eafbfaa8943caea6dd3861
martsenko97 <amarcenko1997@gmail.com>
2023-11-06 18:47:21 +0200

    Some fix

--- task10-removeElement/script.js
+++ task10-removeElement/script.js
@@ -1,13 +1,15 @@
-'use strict';
-
 function removeElement(array, item) {
-	const index = array.indexOf(item);
-	if (index !== -1) {
-		array.splice(index, 1);
+	for (let i = array.length - 1; i >= 0; i--) {
+		if (
+			array[i] === item ||
+			(Array.isArray(item) && JSON.stringify(array[i]) === JSON.stringify(item))
+		) {
+			array.splice(i, 1);
+		}
 	}
 }
 
 const array = [1, 2, 3, 4, 5, 'string', [1, 2, 3]];
-const newItem = 5;
+const newItem = [1, 2, 3];
 removeElement(array, newItem);
 console.log(array);
